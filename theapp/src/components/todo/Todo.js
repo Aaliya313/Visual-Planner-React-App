5199705f27ede11ce6947edb9ba21be37dc9db90
tina.g <77801352+cristinabadea@users.noreply.github.com>
2023-03-15 22:18:46 +0000

    Added the todo functionality

---
+++ theapp/src/components/todo/Todo.js
@@ -0,0 +1,53 @@
+/** @format */
+
+import React, { useState } from "react";
+import { Alert, Card, Button, Form, Container } from "react-bootstrap";
+import "../todo/style.css";
+
+function Todo() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = () => {
+    if (inputValue !== "") {
+      setTodos([...todos, inputValue]);
+      setInputValue("");
+    }
+  };
+  const handleRemoveTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <Container>
+      <Form className="main-todo-form">
+        <Form.Control
+          className="todo-form-item"
+          id="item1"
+          type="text"
+          placeholder="Enter new item"
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+        />
+        <Button className="todo-form-item" onClick={handleAddTodo}>
+          Add Todo
+        </Button>
+      </Form>
+      <ul className="todos-list">
+        {todos.map((todo, index) => (
+          <Card key={index} style={{ width: "18rem" }}>
+            <Card.Body className="todo-body">
+              <Card.Title> {todo}</Card.Title>
+              <Button>Add to calendar!</Button>
+              <Button onClick={() => handleRemoveTodo(index)}>Remove</Button>
+            </Card.Body>
+          </Card>
+        ))}
+      </ul>
+    </Container>
+  );
+}
+
+export default Todo;
--- theapp/src/pages/plannerPage/Planner.js
+++ theapp/src/pages/plannerPage/Planner.js
@@ -1,11 +1,18 @@
 /** @format */
 
 import React from "react";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import Todo from "../../components/todo/Todo";
 
 function Planner() {
   return (
     <div>
-      <p>Second page yeeey</p>
+      <Header />
+      <br />
+      <Todo />
+      <br />
+      <Footer />
     </div>
   );
 }
