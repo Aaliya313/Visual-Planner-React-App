dc6e7a495347cab3e91a7d27b007a7e5b822bddd
tina.g <77801352+cristinabadea@users.noreply.github.com>
2023-03-15 19:52:28 +0000

    Added react-router for planner page

--- theapp/src/App.js
+++ theapp/src/App.js
@@ -1,21 +1,18 @@
 /** @format */
-
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Header from "./components/header/Header";
-import CarouselSection from "./components/carousel/Carousel";
-import MainCards from "./components/mainCards/mainCards";
-import Footer from "./components/footer/Footer";
+import Homepage from "./pages/homepage/Homepage";
+import Planner from "./pages/plannerPage/Planner";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <CarouselSection />
-      <br />
-      <MainCards />
-      <br />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/homepage" element={<Homepage />}></Route>
+        <Route path="/Planner" element={<Planner />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ theapp/src/pages/plannerPage/Planner.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+
+function Planner() {
+  return (
+    <div>
+      <p>Second page yeeey</p>
+    </div>
+  );
+}
+
+export default Planner;
