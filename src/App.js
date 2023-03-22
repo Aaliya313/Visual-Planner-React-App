9a815be00ba9328469036d43feecfe669382aec2
Aaliya313 <aaliyaladha@hotmail.co.uk>
2023-03-22 21:11:28 +0000

    adding new node modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /** @format */
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
 import MeetingPage from "./pages/meetingsPage/MeetingPage";
@@ -7,13 +7,13 @@ import Planner from "./pages/plannerPage/Planner";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/Planner" element={<Planner />}></Route>
         <Route path="/meeting" element={<MeetingPage />}></Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
